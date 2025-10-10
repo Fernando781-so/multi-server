a88332304ed239b8fe78b0f3f64a1825417780c9
fernando <simon@example.com>
2025-10-10 13:02:02 -0700

    see PR

--- Servidor Asincrono/src/ClienteMulti/paraRecibir.java
+++ Servidor Asincrono/src/ClienteMulti/paraRecibir.java
@@ -6,27 +6,21 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class paraRecibir implements Runnable {
-   final DataInputStream entrada;
+    private DataInputStream entrada;
+
     public paraRecibir(Socket s) throws IOException {
-        entrada = new DataInputStream(s.getInputStream());
+        this.entrada = new DataInputStream(s.getInputStream());
     }
 
     @Override
     public void run() {
-        String mensaje = "";
-        while(true){
-            try {
-                mensaje = entrada.readUTF();
-                int idx = mensaje.indexOf(": ");
-                if (idx != -1) {
-                    String remitente = mensaje.substring(0, idx);
-                    String contenido = mensaje.substring(idx + 2);
-                    System.out.println("Mensaje de " + remitente + ": " + contenido);
-                } else {
-                    System.out.println(mensaje);
-                }
-            } catch (IOException ex) {
-      }
-    } 
-  } 
+        try {
+            while (true) {
+                String mensaje = entrada.readUTF();
+                System.out.println(mensaje);
+            }
+        } catch (IOException e) {
+            System.out.println("Desconectado del servidor.");
+        }
+    }
 }
