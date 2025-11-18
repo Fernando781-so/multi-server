b8a0364b3913f94b3d9981bd48d84c1cb64c9394
fernando <simon@example.com>
2025-11-17 18:51:21 -0700

    testeo

--- Servidor Asincrono/src/ClienteMulti/ClienteMulti.java
+++ Servidor Asincrono/src/ClienteMulti/ClienteMulti.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 public class ClienteMulti {
     public static void main(String[] args) {
-        String host = "192.168.137.1"; 
+        String host = "localhost"; 
         int port = 8080;
         try {
             Socket s = new Socket(host, port);
