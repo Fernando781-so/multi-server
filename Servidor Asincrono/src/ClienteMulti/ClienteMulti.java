3b0bf95322aefb4b7b1fb628b67f18025a24aac4
fernando <simon@example.com>
2025-11-17 17:44:58 -0700

    El cliente para comadrear

--- Servidor Asincrono/src/ClienteMulti/ClienteMulti.java
+++ Servidor Asincrono/src/ClienteMulti/ClienteMulti.java
@@ -5,26 +5,25 @@ import java.net.Socket;
 
 public class ClienteMulti {
     public static void main(String[] args) {
-        String host = "LocalHost"; 
+        String host = "192.168.137.1"; 
         int port = 8080;
         try {
             Socket s = new Socket(host, port);
-            System.out.println("Conectado al servidor.");
+            System.out.println("Conectado al servidor " + host + ":" + port);
 
-            paraMandar paraMandar = new paraMandar(s);
-            Thread hiloParaMandar = new Thread(paraMandar);
-            hiloParaMandar.start();
+            paraMandar pm = new paraMandar(s);
+            Thread t1 = new Thread(pm);
+            t1.start();
 
-            paraRecibir paraRecibir = new paraRecibir(s);
-            Thread hiloParaRecibir = new Thread(paraRecibir);
-            hiloParaRecibir.start();
+            paraRecibir pr = new paraRecibir(s);
+            Thread t2 = new Thread(pr);
+            t2.start();
 
-        } catch (IOException ex) {
-            System.out.println("❌ No se pudo conectar al servidor (" + host + ":" + port + ").");
+        } catch (IOException e) {
+            System.out.println("No se pudo conectar al servidor en " + host + ":" + port + " -> ");
             System.out.println("Cerrando cliente.");
-            // salir limpiamente
+            // salir sin crashear
         }
     }
 }
-
 
